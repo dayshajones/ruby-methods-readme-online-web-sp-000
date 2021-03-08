ec6b112af353e040409923aa4df628674dfad02e
Daysha Jones <daysha.vu@icloud.com>
2021-03-08 21:12:56 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,15 +1,5 @@
 # Use this space to code along with the readme
 
 def greeting
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+puts "Hello World!"
 end
